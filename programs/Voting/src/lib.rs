e6f8e367c3977d54ab771dc86c1db077838e94fe
Gajesh Naik <robogajesh@gmail.com>
2021-11-29 00:05:52 +0530

    gm solana, gm

--- programs/Voting/src/lib.rs
+++ programs/Voting/src/lib.rs
@@ -1,4 +1,5 @@
 use {anchor_lang::prelude::*, anchor_lang::solana_program::account_info::AccountInfo};
+use anchor_lang::solana_program::sysvar::clock::Clock;
 declare_id!("GRar9nHYoMPyrtiWHtW7AjRWWdFrBk7PAEQbUmEP8LUe");
 
 #[program]
@@ -17,57 +18,63 @@ pub mod voting {
 
         for address in voters.iter() {
             // finding address in voters array
+            let mut exists = false;
+
             for (index, voter) in base_account.voters.iter().enumerate() {
                 if voter.address == *address {
                     base_account.voters[index].is_voter = true;
+                    exists = true;
                     break;
                 }
+            }
+            
+            if !exists {
+                base_account.voters.push(Voters {
+                    address: *address,
+                    is_voter: true,
+                });
             }
         }
 
         Ok(())
     }
 
-    pub fn create_proposal(
+    pub fn create_ballot(
         ctx: Context<OwnerContext>,
         name: String,
         desc: String,
         choices: Vec<String>,
-        offset: u128,
+        offset: u32,
     ) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
         let next_proposal_id = base_account.next_proposal_id;
+        let mut choice_array: Vec<Choice> = vec![];
 
-        for (index, proposal) in base_account.proposals.iter().enumerate() {
-            if proposal.id == next_proposal_id {
-                let mut choice_array: Vec<Choice> = vec![];
-
-                for (index, choice) in choices.iter().enumerate() {
-                    choice_array.push(Choice {
-                        id: index as u128,
-                        name: choice.clone(),
-                        votes: 0,
-                    });
-                }
-
-                base_account.proposals[index].proposal = Proposal {
-                    id: next_proposal_id,
-                    proposal: name,
-                    description: desc,
-                    end_time_stamp: Clock::get()?.unix_timestamp as u128 + offset,
-                    choices: choice_array.clone(),
-                };
-
-                break;
-            }
+        for (index, choice) in choices.iter().enumerate() {
+            choice_array.push(Choice {
+                id: index as u128,
+                name: choice.clone(),
+                votes: 0,
+            });
         }
+
+        base_account.proposals.push(ProposalArray {
+            id: next_proposal_id,
+            proposal: Proposal {
+                id: next_proposal_id,
+                proposal: name,
+                description: desc,
+                end_time_stamp: Clock::get().unwrap().unix_timestamp as u128 + offset as u128,
+                choices: choice_array.clone(),
+            },
+        });
 
         base_account.next_proposal_id += 1;
 
         Ok(())
     }
 
-    pub fn vote(ctx: Context<VoteContext>, proposal_id: u128, choice_id: u128) -> ProgramResult {
+    pub fn vote(ctx: Context<VoteContext>, proposal_id: u32, choice_id: u32) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
         let msg_sender = ctx.accounts.msg_sender.key();
 
@@ -79,15 +86,19 @@ pub mod voting {
         }
 
         let mut address_index = 0;
+        let mut address_exists = false;
+        let mut proposal_exists = false;
         let mut votes_index = 0;
         let mut proposal_index = 0;
 
         for (address_idx, voter) in base_account.votes.iter().enumerate() {
             if voter.address == msg_sender {
+                address_exists = true;
                 for (votes_idx, votes) in voter.votes_util.iter().enumerate() {
-                    if votes.proposal == proposal_id {
+                    if votes.proposal == proposal_id as u128 {
                         address_index = address_idx;
                         votes_index = votes_idx;
+                        proposal_exists = true;
                         break;
                     }
                 }
@@ -95,19 +106,40 @@ pub mod voting {
             }
         }
 
+        if !address_exists {
+            base_account.votes.push(Votes {
+                address: msg_sender,
+                votes_util: vec![VotesUtils {
+                    proposal: proposal_id as u128,
+                    voted: false,
+                }],
+            });
+            address_index = base_account.votes.len() - 1;
+        }
+
+        if !proposal_exists {
+            base_account.votes[address_index]
+                .votes_util
+                .push(VotesUtils {
+                    proposal: proposal_id as u128,
+                    voted: false,
+                });
+            votes_index = base_account.votes[address_index].votes_util.len() - 1;
+        }
+
         assert!(
-            base_account.votes[address_index].votes_util[votes_index].voted,
+            !base_account.votes[address_index].votes_util[votes_index].voted,
             "vote:: voter can only vote once for a ballot"
         );
 
         for (index, proposal) in base_account.proposals.iter().enumerate() {
-            if proposal.id == proposal_id {
+            if proposal.id == proposal_id as u128 {
                 proposal_index = index;
             }
         }
 
         assert!(
-            (Clock::get()?.unix_timestamp as u128)
+            (Clock::get().unwrap().unix_timestamp as u128)
                 < base_account.proposals[proposal_index]
                     .proposal
                     .end_time_stamp,
@@ -172,7 +204,6 @@ pub struct ProposalArray {
     pub id: u128,
     pub proposal: Proposal,
 }
-
 
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct VotesUtils {
